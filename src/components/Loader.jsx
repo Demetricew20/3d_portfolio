e7b30d9abdab5a5bc97b0fd383d42d70726dcfa5
DemetriceW20 <demetrice_williams@hotmail.com>
2023-12-16 15:16:43 -0600

    add assets for 3d rendering. created island component.

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { Html } from "@react-three/drei";
 
 const Loader = () => {
   return (
-    <div className='flex justify-center items-center'>
-        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"></div>      
-    </div>
-  )
-}
+    // Need to fix this, loader cannot use div since it lives as a fallback for the 3d Canvas
+    // 3d Canvas cannot use divs
 
-export default Loader
+    <div className="flex justify-center items-center">
+      <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
