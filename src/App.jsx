3c1188624c24c52368e02060be766a5656165e98
DemetriceW20 <demetrice_williams@hotmail.com>
2023-12-17 13:05:31 -0600

    add react vetcal timeline component. updated about page with work info

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <main className="bg-slate-300/20 lg:h-screen">
+    <main className="bg-slate-300/20 ">
       <Router>
         <Navbar />
         <Routes>
